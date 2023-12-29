1e2b2bf88f304ba8f1e85f9e409f4b9969045770
jhy <1194345103@qq.com>
2023-12-29 19:39:08 +0800

    formal version

--- run_cifar10.sh
+++ run_cifar10.sh
@@ -1,9 +1,8 @@
-
 torchrun --standalone --nnodes=1 --nproc-per-node=4 main.py  --dataset cifar10 --num-labeled  400 --out myResult --arch wideresnet --lambda_oem 0.1 --lambda_socr 0.5 \
 --batch-size 64 --lr 0.03 --expand-labels --seed 0 --opt_level O2  --mu 2
-# $1  每类标记数据数量
-# $2   输出结果的位置（文件夹名）
-# #3    CUDA设备
+#   $1  每类标记数据数量
+#   $2   输出结果的位置（文件夹名）
+#   $3    CUDA设备
 
 
 

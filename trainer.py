84a5ded41611144189c03bf9b20550033cd9277f
jhy <1194345103@qq.com>
2023-12-30 11:15:34 +0800

    测试代码更新

--- trainer.py
+++ trainer.py
@@ -179,8 +179,9 @@ def train(args, labeled_trainloader, unlabeled_dataset, test_loader, val_loader,
                 with amp.scale_loss(loss, optimizer) as scaled_loss:
                     scaled_loss.backward()
             else:
-                torch.autograd.set_detect_anomaly(True)
-                loss.backward()
+                with torch.autograd.set_detect_anomaly(True):
+
+                    loss.backward()
 
             losses.update(loss.item())
             losses_x.update(Lx.item())

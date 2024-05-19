fc595441ba36f06af5afe7206b7a35f868ae1444
jhy <1194345103@qq.com>
2024-05-19 21:48:24 +0800

    bug fixed

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ from utils import set_parser
 args = set_parser()
 for k,v in vars(args).items():
     print(f"{k}:{v}")
-# print(args)
+print(args)
 print("__________________")
 print(type(args))
 # print(NameSpace)
